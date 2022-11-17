af5e24c7a3a2a0c7d512002643fb65ea7ab41739
Jesus Castillo <jesus.castillohdz98@gmail.com>
2022-11-16 22:55:02 -0600

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      host: env("PGHOST", "containers-us-west-130.railway.app"),
+      port: env.int("PGPORT", 6707),
+      database: env("PGDATABASE", "railway"),
+      user: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "dfddjYopZnCevWdIZQuK"),
       ssl: env.bool(true),
     },
   },
